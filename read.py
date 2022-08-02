c038557e8ad64a8d08b3547978153d06fb6592a0
Ian Hong <hongreian@hotmail.com>
2022-08-02 22:14:38 +0800

    add more filter with different method

--- read.py
+++ read.py
@@ -18,3 +18,19 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('總共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+if 'good' in d:
+	good.append(d)
+print('一共有', len(good), '筆留言提到good')		
+# 上面等於下面寫法
+good = [d for d in data if 'good' in d]
+print(good)
+
+bad = ['bad' in d for d in data]
+print(bad)
+# 上面等於下面寫法
+bad = []
+for d in date:
+	bad.append('bad' in d)
